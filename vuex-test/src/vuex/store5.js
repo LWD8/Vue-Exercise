f4ff4be66139cc689c8051f68ae00ebca28acf02
LWD8 <liwendiweb@foxmail.com>
2018-01-13 17:22:15 +0800

    2018-1-13

---
+++ vuex-test/src/vuex/store5.js
@@ -0,0 +1,44 @@
+import vue from 'vue'
+import vuex from 'vuex'
+vue.use(vuex)
+
+const state = {
+    count: 3
+}
+
+const mutations = {  //mutation 突变，改变
+    add(state,n) {      //n是add方法传入的参数
+        state.count += n
+    },
+    reduce(state,n) {
+        state.count -= n
+    }
+}
+
+//每操作一次count给count加100，不管加还是减   
+const getters = { 
+    count(state) {
+        return state.count += 100;
+    }
+}
+
+const actions = {
+    addAction(context) {
+        console.log('add');
+        context.commit('add',10)
+        setTimeout(()=>{
+            context.commit('reduce',1)  //异步
+        },1000 )
+    },
+    reduceAction({commit}) {
+        console.log('reduce');
+        commit('reduce',5)   // 提交给突变mutations
+    }
+}
+
+export default new vuex.Store({
+    state,
+    mutations,
+    getters,
+    actions
+})
